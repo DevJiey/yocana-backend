b5867b41f4bc4d6394700756deedcda58b7f8df6
Ronald Jay Cruz <ronaldcruz070103@gmail.com>
2026-08-30 19:40:10 +0800

    Fix SMTP IPv4 connection

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,4 +1,7 @@
 const nodemailer = require("nodemailer");
+const dns = require("node:dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
